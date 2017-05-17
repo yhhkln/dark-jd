9a0bb6984ef6e32571fe6e0e5c4b1a944e8205c4
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-05-17 22:13:15 +0800

    yml search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   # before_action :authenticate_user!, :only, [:new, :edit, :create, :update, :destroy]
-
+  before_action :validate_search_key, only: [:search]
   def index
     @products = Product.all.order("position ASC")
   end
@@ -51,6 +51,32 @@ class ProductsController < ApplicationController
     end
     redirect_to :back
   end
+
+
+# 搜索
+  def search
+    if @query_string.present?
+      search_result = Product.ransack(@search_criteria).result(:distinct => true)
+      @products = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
+    if params[:q].present?
+      @search_criteria = search_criteria(@query_string)
+    end
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+
+# 搜索
 
   private
 
