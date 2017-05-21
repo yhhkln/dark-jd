ee3f63aeceaeb2d5dee0007f0c134e07dddbb50b
谢育初 <524945638@qq.com>
2017-05-21 21:03:32 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users, :controllers => { :registrations => "users/registrations" }, :controllers => { :sessions => "users/sessions" }
+  devise_for :users, :controllers => { :registrations => "users/registrations", :sessions => "users/sessions" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products do
